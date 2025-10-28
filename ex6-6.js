80827ebb526f70f6f0b8f74cad1cd133b8fa89a7
kaito117 <zm24346@josai.ac.jp>
2025-10-28 10:11:26 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,5 +1,5 @@
-var who65='world';
-function sayhello65(){alert('Hello,'+who65+'!');}
-function taro65(){who65='太郎';}
-function hanako65(){who65='花子';}
-function sayhello65(){alert('who65+'が来た');}
+var who66='world';
+function sayhello66(){alert('Hello,'+who65+'!');}
+function taro65(){who66='太郎';}
+function hanako65(){who66='花子';}
+function sayhello66(){alert('who65+'が来た');}
