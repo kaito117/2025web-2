2eea4782b2e28f8429de2a8253ba449a588fd211
kaito117 <zm24346@josai.ac.jp>
2025-12-02 10:10:55 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,6 +1,6 @@
 function imgchange1(){
-    document.getElementById('logo').src = './image/urawa2.jpg';
+    document.getElementById('logo').src = './image/urawa2.JPG';
 }
 function imgchange2(){
-    document.getElementById('logo').src = './image/urawa.jpg';
+    document.getElementById('logo').src = './image/urawa.JPG';
 }
