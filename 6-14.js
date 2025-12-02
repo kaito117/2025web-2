92fb74e0b8872a1f32c1afbe1e0be703e4825b9e
kaito117 <zm24346@josai.ac.jp>
2025-12-02 10:08:25 +0900

    Create 6-14.js

---
+++ 6-14.js
@@ -0,0 +1,6 @@
+function imgchange1(){
+    document.getElementById('logo').src = './image/photo1.jpg';
+}
+function imgchange2(){
+    document.getElementById('logo').src = './image/photo2.jpg';
+}
