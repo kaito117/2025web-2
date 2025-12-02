4a49696652ded12ea127e5949385c71fe2828dd9
kaito117 <zm24346@josai.ac.jp>
2025-12-02 11:21:57 +0900

    Update 6-14.js

--- 6-14.js
+++ 6-14.js
@@ -1,7 +1,7 @@
 function changePhoto(){
-    document.getElementById('logo').src = './image/photo1.jpg';
-}
-function resetPhoto(){
-    document.getElementById('logo').src = './image/photo2.jpg';
+    document.getElementById('face').src = './image/photo2.JPG';
 }
 
+function resetPhoto(){
+    document.getElementById('face').src = './image/photo1.JPG';
+}
