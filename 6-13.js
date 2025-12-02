5e57006bcd293338b4f28c0e259556ec798a1021
kaito117 <zm24346@josai.ac.jp>
2025-12-02 10:24:26 +0900

    Update 6-13.js

--- 6-13.js
+++ 6-13.js
@@ -1,6 +1,6 @@
-function imgchange1(){
+function imgchange3(){
     document.getElementById('logo').src = './image/城西ロゴ1.jpg';
 }
-function imgchange2(){
+function imgchange4(){
     document.getElementById('logo').src = './image/城西ロゴ2.jpg';
 }
