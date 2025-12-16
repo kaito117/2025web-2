4989e43e978364ad51434c6c44f5be779e231385
kaito117 <zm24346@josai.ac.jp>
2025-12-16 09:43:16 +0900

    Update ex6-17.js

--- ex6-17.js
+++ ex6-17.js
@@ -1,4 +1,4 @@
-function hyojhy {var str17 = "一文字ずつ表示します。";
+function hyoji17 {var str17 = "一文字ずつ表示します。";
     var cnt17 = document.timer17.moji17.value.length;
     if (cnt17 < 11) {document.timer17.moji17.value = str17.substr(0, cnt17 + 1);}
     else {document.timer17.moji17.value = ""; }
