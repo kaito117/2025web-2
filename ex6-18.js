b36e7001236456b0ceaad94bc773a2f5ed6b1063
kaito117 <zm24346@josai.ac.jp>
2025-12-16 10:09:38 +0900

    Create ex6-18.js

---
+++ ex6-18.js
@@ -0,0 +1,6 @@
+  function changeImage() {const img = document.getElementById("img18");
+    img.src = "サッカーボール2.jpg";
+    setTimeout(function () {
+      img.src = "サッカーボール1.jpg";
+    }, 3000);}
+
