d5d37b5ccbf8b0ef4d010f966efa60bb164008e9
kaito117 <zm24346@josai.ac.jp>
2026-01-27 17:41:04 +0900

    Add function to check user input for correct answer

---
+++ ex7-9.js
@@ -0,0 +1,13 @@
+function nazo79() {
+  const input = document.getElementById('answer79').value;
+
+  if (input === "夢") {
+    alert("あたり！");
+  } else {
+    if (input === "ゆめ") {
+      alert("あたり！");
+    } else {
+      alert("はずれ！");
+    }
+  }
+}
