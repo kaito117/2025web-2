a3edd471897b4089a5c5234e90a51a2077d57a3c
kaito117 <zm24346@josai.ac.jp>
2026-01-29 12:50:25 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,10 +1,10 @@
 function checkYear() {
   let year = Number(document.getElementById("year").value);
-  let result = document.getElementById("result");
+  let result1 = document.getElementById("result");
 
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    result.textContent = year + "年は「うるう年」です";
+    result1.textContent = year + "年は「うるう年」です";
   } else {
-    result.textContent = year + "年は「平年」です";
+    result1.textContent = year + "年は「平年」です";
   }
 }
